c0f2edb74ce3ac0db0430352b5b270ec4f0dc201
Alfonsocruzorozco <alfonsocruzorozco@gmail.com>
2026-04-20 11:46:37 -0600

    feat: initial project setup with SwiftUI

---
+++ Bible-Cristiana/Bible-Cristiana/Bible_CristianaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bible_CristianaApp.swift
+//  Bible-Cristiana
+//
+//  Created by Alfonso Cruz Orozco on 20/04/26.
+//
+
+import SwiftUI
+
+@main
+struct Bible_CristianaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
